8b35ea3c947d0efc14487fdf84e1d79c8a11c137
PRASHANNA RAI <prai931024@gmail.com>
2025-04-02 13:46:19 +0200

    working GraphQL endpoints

--- index.js
+++ index.js
@@ -1,99 +1,147 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "@apollo/server";
 import neo4j from "neo4j-driver";
 import { Neo4jGraphQL } from "@neo4j/graphql";
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const resolvers = {
-  Business: {
-    waitTime: (obj, args, context, info) => {
-      var options = [0, 5, 10, 15, 30, 45];
-      return options[Math.floor(Math.random() * options.length)];
-    },
-  },
-};
+const NEO4J_HOST = process.env.NEO4J_HOST;
+const NEO4J_PORT = process.env.NEO4J_PORT;
+const NEO4J_USERNAME = process.env.NEO4J_USERNAME;
+const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD;
 
 const typeDefs = /* GraphQL */ `
-  type Query {
-    findBusinessWithCustomEndpoint: Business
-      @cypher(
-        statement: """
-        MATCH (u:User)-[:WROTE]->(r:Review)-[:REVIEWS]->(b:Business) RETURN b
-        """
-      )
+type ActedInProperties @relationshipProperties {
+	roles: [String]!
+}
 
-      findBusinessWithCustomEndpointParameters(farmName: String): Farm
-      @cypher(
-        statement: """
-        match(f:Farm { name: $farmName }) return f
-        """
-      )
-  }
+type Business @node {
+	name: String!
+	reviewsReviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
+}
+
+type Component @node {
+	componentshasSubComponent: [Component!]! @relationship(type: "has_sub_component", direction: IN)
+	containsBluePrintComponentBlueprints: [ComponentBlueprint!]! @relationship(type: "contains_blue_print", direction: OUT)
+	deviceshasComponent: [Device!]! @relationship(type: "has_component", direction: IN)
+	hasSubComponentComponents: [Component!]! @relationship(type: "has_sub_component", direction: OUT)
+	installed_at: Date!
+	label: String!
+	serial_number: BigInt!
+	serviced_at: Date!
+}
+
+type ComponentBlueprint @node {
+	componentBlueprintshasSubComponentBlueprint: [ComponentBlueprint!]! @relationship(type: "has_sub_component_blueprint", direction: IN)
+	componentscontainsBluePrint: [Component!]! @relationship(type: "contains_blue_print", direction: IN)
+	devicescontainsBluePrint: [Device!]! @relationship(type: "contains_blue_print", direction: IN)
+	hasSubComponentBlueprintComponentBlueprints: [ComponentBlueprint!]! @relationship(type: "has_sub_component_blueprint", direction: OUT)
+	hotspotsmarksLocationOf: [Hotspot!]! @relationship(type: "marks_location_of", direction: IN)
+	id: String!
+	label: String!
+}
+
+type Device @node {
+	containsBluePrintComponentBlueprints: [ComponentBlueprint!]! @relationship(type: "contains_blue_print", direction: OUT)
+	farmshasDevice: [Farm!]! @relationship(type: "has_device", direction: IN)
+	hardware_version: Float!
+	hasComponentComponents: [Component!]! @relationship(type: "has_component", direction: OUT)
+	hasDeviceTypeDeviceTypes: [DeviceType!]! @relationship(type: "has_device_type", direction: OUT)
+	installed_at: Date!
+	label: String!
+	serial_number: BigInt!
+	serviced_at: Date!
+}
+
+type DeviceType @node {
+	deviceshasDeviceType: [Device!]! @relationship(type: "has_device_type", direction: IN)
+	name: String!
+}
+
+type Farm @node {
+	hasDeviceDevices: [Device!]! @relationship(type: "has_device", direction: OUT)
+	name: String!
+}
+
+type Hotspot @node {
+	file: String!
+	id: String!
+	linkId: String!
+	marksLocationInImages: [Image!]! @relationship(type: "marks_location_in", direction: OUT)
+	marksLocationOfComponentBlueprints: [ComponentBlueprint!]! @relationship(type: "marks_location_of", direction: OUT)
+}
+
+type Image @node {
+	hotspotsmarksLocationIn: [Hotspot!]! @relationship(type: "marks_location_in", direction: IN)
+	path: String!
+}
+
+type Movie @node {
+	peopleActedIn: [Person!]! @relationship(type: "ACTED_IN", direction: IN, properties: "ActedInProperties")
+	peopleDirected: [Person!]! @relationship(type: "DIRECTED", direction: IN)
+	peopleProduced: [Person!]! @relationship(type: "PRODUCED", direction: IN)
+	peopleReviewed: [Person!]! @relationship(type: "REVIEWED", direction: IN, properties: "ReviewedProperties")
+	peopleWrote: [Person!]! @relationship(type: "WROTE", direction: IN)
+	released: BigInt!
+	tagline: String
+	title: String!
+}
+
+type Person @node {
+	actedInMovies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT, properties: "ActedInProperties")
+	born: BigInt
+	directedMovies: [Movie!]! @relationship(type: "DIRECTED", direction: OUT)
+	followsPeople: [Person!]! @relationship(type: "FOLLOWS", direction: OUT)
+	name: String!
+	peopleFollows: [Person!]! @relationship(type: "FOLLOWS", direction: IN)
+	producedMovies: [Movie!]! @relationship(type: "PRODUCED", direction: OUT)
+	reviewedMovies: [Movie!]! @relationship(type: "REVIEWED", direction: OUT, properties: "ReviewedProperties")
+	wroteMovies: [Movie!]! @relationship(type: "WROTE", direction: OUT)
+}
+
+type Review @node {
+	reviewsBusinesses: [Business!]! @relationship(type: "REVIEWS", direction: OUT)
+	stars: BigInt!
+	text: String!
+	usersWrote: [User!]! @relationship(type: "WROTE", direction: IN)
+}
+
+type ReviewedProperties @relationshipProperties {
+	rating: BigInt!
+	summary: String!
+}
 
 
 
+type User @node {
+	name: String!
+	wroteReviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
+}
 
-  type Business {
-    businessId: ID!
-    waitTime: Int! @customResolver
-    averageStars: Float!
-      @cypher(
-        statement: "MATCH (this)<-[:REVIEWS]-(r:Review) RETURN avg(r.stars)"
-      )
-    recommended(first: Int = 1): [Business!]!
-      @cypher(
-        statement: """
-        MATCH (this)<-[:REVIEWS]-(:Review)<-[:WROTE]-(:User)-[:WROTE]->(:Review)-[:REVIEWS]->(rec:Business)
-        WITH rec, COUNT(*) AS score
-        RETURN rec ORDER BY score DESC LIMIT $first
-        """
-      )
-    name: String!
-    city: String!
-    state: String!
-    address: String!
-    location: Point!
-    reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
-    categories: [Category!]! @relationship(type: "IN_CATEGORY", direction: OUT)
-  }
 
-  type User {
-    userID: ID!
-    name: String!
-    reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
-  }
-
-  type Farm {
-    name: String
-  }
-
-  type Review {
-    reviewId: ID!
-    stars: Float!
-    date: Date!
-    text: String
-    user: User! @relationship(type: "WROTE", direction: IN)
-    business: Business! @relationship(type: "REVIEWS", direction: OUT)
-  }
-
-  type Category {
-    name: String!
-    businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
-  }
 `;
 
 const driver = neo4j.driver(
-  "bolt://localhost:7687",
-  neo4j.auth.basic("neo4j", "secretgraph")
+  `bolt://${NEO4J_HOST}:${NEO4J_PORT}`,
+  neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
 );
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, resolvers, driver });
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
-neoSchema.getSchema().then((schema) => {
-  const server = new ApolloServer({
-    schema,
-  });
-  server.listen().then(({ url }) => {
-    console.log(`GraphQL server ready at ${url}`);
-  });
-}).catch((error) => {
-  console.log("Error:", error);
-});
+neoSchema.getSchema()
+    .then(schema => {
+        const server = new ApolloServer({
+            schema: schema,
+        });
+
+        return startStandaloneServer(server, {
+            context: ({ req }) => ({ req }),
+            listen: { port: 4000 },
+        });
+    })
+    .then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+    })
+    .catch(error => {
+        console.error('Error starting the server:', error);
+    });
+
+
--- introspect.js
+++ introspect.js
@@ -1,7 +1,15 @@
-const { toGraphQLTypeDefs } = require("@neo4j/introspector");
-const neo4j = require("neo4j-driver");
-const fs = require("fs");
-const driver = neo4j.driver("neo4j://localhost:7687", neo4j.auth.basic("neo4j", "secretgraph"));
+import { toGraphQLTypeDefs } from "@neo4j/introspector";
+import neo4j from "neo4j-driver";
+import fs from "fs";
+
+const NEO4J_HOST = process.env.NEO4J_HOST;
+const NEO4J_PORT = process.env.NEO4J_PORT;
+const NEO4J_USERNAME = process.env.NEO4J_USERNAME;
+const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD;
+
+const driver = neo4j.driver(
+    `bolt://${NEO4J_HOST}:${NEO4J_PORT}`, 
+    neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD));
 const sessionFactory = () => driver.session({ defaultAccessMode: neo4j.session.READ });
 // We create a async function here until "top level await" has landed
 // so we can use async/await
